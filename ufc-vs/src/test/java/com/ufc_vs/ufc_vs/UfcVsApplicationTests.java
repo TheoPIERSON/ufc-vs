67a189a50edc9b68fba615fa57d2dcc070ab8d23
Theo Pierson <thheopierson@gmail.com>
2024-10-23 12:40:17 +0200

    first commit

---
+++ ufc-vs/src/test/java/com/ufc_vs/ufc_vs/UfcVsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc_vs.ufc_vs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcVsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
